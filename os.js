217758f7e635da93826fb81c39cb4466e6e0bca8
Pranat Dayal <pranat.dayal@gmail.com>
2018-11-14 13:57:55 -0500

    Added method to retrieve IP address

--- os.js
+++ os.js
@@ -6,3 +6,17 @@ function get_os(){
 }
 
 get_os();
+
+window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;//compatibility for Firefox and chrome
+var pc = new RTCPeerConnection({iceServers:[]}), noop = function(){};
+pc.createDataChannel('');//create a bogus data channel
+pc.createOffer(pc.setLocalDescription.bind(pc), noop);// create offer and set local description
+pc.onicecandidate = function(ice)
+{
+ if (ice && ice.candidate && ice.candidate.candidate)
+ {
+  var myIP = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
+  document.write('my IP: ', myIP);
+  pc.onicecandidate = noop;
+ }
+};
